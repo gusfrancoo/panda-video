9f1695d46743ec4cfce200c22f5043f3defc0dde
gusfrancoo <gustafranco2543@gmail.com>
2025-06-08 13:15:29 -0300

    ✨ feat: criação layout navbar

---
+++ front-end/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-container>
+      <v-row class="d-flex">
+        <v-toolbar-title>Panda Video</v-toolbar-title>
+        <v-spacer />
+        <v-btn text to="/home">Home</v-btn>
+        <v-btn append-icon="mdi-logout" text @click="onLogout">Logout</v-btn>
+      </v-row>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script setup>
+  import { useRouter } from 'vue-router'
+  // import { logout } from '@/api/auth.api'
+
+  const router = useRouter()
+
+  async function onLogout () {
+    localStorage.removeItem('token')
+    router.push('/login')
+  }
+</script>
--- front-end/src/layouts/default.vue
+++ front-end/src/layouts/default.vue
@@ -1,10 +1,14 @@
 <template>
   <v-main>
+    <Navbar v-if="route.path !== '/login'" />
     <router-view />
   </v-main>
 
 </template>
 
 <script setup>
-  //
+  import { useRoute } from 'vue-router'
+  import Navbar from '@/components/Navbar.vue'
+
+  const route = useRoute()
 </script>
