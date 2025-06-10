e0f118474cdb121f16d19f1b02eda3132850d762
gusfrancoo <gustafranco2543@gmail.com>
2025-06-10 09:36:24 -0300

    💄 feat: criando empty states

---
+++ front-end/src/pages/[...all].vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container class="fill-height d-flex align-center justify-center">
+    <v-card class="pa-5 text-center elevation-2 rounded">
+      <v-col class="py-5">
+        <v-icon color="grey-darken-2" size="64">mdi-alert-circle-outline</v-icon>
+        <h2>Página não encontrada</h2>
+      </v-col>
+      <v-btn color="primary" @click="$router.push('/home')">
+        Voltar para Home
+      </v-btn>
+    </v-card>
+  </v-container>
+</template>
---
+++ front-end/src/pages/unauthorized.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="fill-height d-flex align-center justify-center">
+    <v-card class="pa-6 text-center">
+      <v-icon color="grey lighten-1" size="64">mdi-lock-outline</v-icon>
+      <h2>Acesso negado</h2>
+      <p>Você não tem permissão para acessar esta página.</p>
+      <v-btn color="primary" @click="$router.push('/login')">
+        Fazer login
+      </v-btn>
+    </v-card>
+  </v-container>
+</template>
