ed162f9e4f15141488a74a4c2614dda3deb8ee8b
gusfrancoo <gustafranco2543@gmail.com>
2025-06-27 19:39:47 -0300

    💄 feat: ajustando responsividade

--- front-end/src/pages/Home.vue
+++ front-end/src/pages/Home.vue
@@ -3,129 +3,234 @@
   <v-container class="">
     <div class="items-center w-100  d-flex justify-center">
 
-      <v-card class="pa-10 rounded-lg w-100 overflow-auto " style="max-width: 1200px; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);">
+      <v-card
+        class="pa-5 rounded-lg w-100 overflow-auto "
+        style="
+          max-width: 1200px;
+          min-height: 600px;
+          max-height: 80vh;
+          box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+          overflow-y: auto;
+        "
+      >
 
         <div class="px-2 mb-5">
           <span class="text-h6" style="font-weight: bold;">Seus Videos</span>
         </div>
-        <v-expand-transition>
-          <div v-if="folderHistory.length > 0" class="px-2">
-            <v-btn
-              class="text-capitalize"
-              color="grey-darken-4"
-              elevation="2"
-              prepend-icon="mdi-arrow-left"
-              variant="elevated"
-              @click="backButton"
-            >
-              Voltar
-            </v-btn>
-          </div>
-        </v-expand-transition>
-        <v-row class="px-2 mb-4 justify-space-between" :class="[currentFolder != null ? 'mt-4' : 'mt-0']" no-gutters>
-          <v-col class="d-flex align-center " cols="12" md="4">
+        <div class="py-2">
+          <v-expand-transition>
+            <div v-if="folderHistory.length > 0" class="px-2">
+              <v-btn
+                class="text-capitalize"
+                color="grey-darken-4"
+                elevation="2"
+                prepend-icon="mdi-arrow-left"
+                variant="elevated"
+                @click="backButton"
+              >
+                Voltar
+              </v-btn>
+            </div>
+          </v-expand-transition>
+        </div>
+        <v-row class="px-2 mb-4" no-gutters>
+          <v-col class="mb-2 mb-md-0" cols="12" md="6">
             <v-text-field
               v-model="search"
               class="w-100"
+              :class="{ 'bg-grey-lighten-3': paginatedItems.length === 0 }"
               :clearable="true"
               density="compact"
+              :disabled="paginatedItems.length === 0"
               hide-details
               placeholder="Filtre por nome de pasta ou vídeo…"
               prepend-inner-icon="mdi-magnify"
-              style="height: 40px; margin: 0;"
               variant="solo"
-              @click:clear="search = ''"
             />
           </v-col>
 
-          <v-col class="d-flex align-center justify-end" cols="12" md="6">
+          <v-col class="d-flex justify-md-end justify-center" cols="12" md="6">
             <v-select
               v-model="itemsPerPage"
-              class="ma-0"
+              class=" w-md-auto"
               density="compact"
               hide-details
               :items="[5, 10, 20, 50]"
               label="Itens por página"
-              style="max-width: 150px; height: 40px;"
+              style="max-width: 150px;"
               variant="solo"
             />
           </v-col>
         </v-row>
-        <v-row class="px-6 w-100 d-flex flex-column" dense>
+        <v-row v-if="paginatedItems.length > 0" class="px-6 w-100 d-flex flex-column" dense>
+          <v-row class="mt-2 w-100 justify-end mb-2">
+            <div class="text-caption text-grey-darken-1">
+              {{ filteredItems.length }} itens encontrados
+            </div>
+          </v-row>
 
-          <v-card
-            v-for="item in paginatedItems"
-            :key="item.id"
-            class="mb-3 pa-4 rounded d-flex align-center cursor-pointer "
-            color="grey-lighten-4"
-            :hover="true"
-            @click="handleRowClick({ item })"
-          >
-            <template v-if="item.isFolder">
-              <v-icon class="mr-4" color="primary" size="48">mdi-folder</v-icon>
-              <div>
-                <div class="text-subtitle-1 font-weight-medium">{{ item.name }}</div>
-                <div class="text-caption text-grey-darken-1">{{ item.count }} vídeos</div>
-              </div>
-            </template>
-
-            <template v-else>
-              <v-img
-                aspect-ratio="16/9"
-                class="rounded mr-4"
-                cover
-                height="80"
-                :src="item.thumbnail"
-                width="140"
+          <!-- DESKTOP -->
+          <v-responsive class="d-none d-sm-flex w-100">
+            <v-col cols="12">
+              <v-card
+                v-for="item in paginatedItems"
+                :key="'desktop-' + item.id"
+                class="mb-3 pa-5 rounded d-flex align-center cursor-pointer"
+                color="grey-lighten-4"
+                :hover="true"
+                @click="handleRowClick({ item })"
               >
-                <template #placeholder>
-                  <v-skeleton-loader height="100" type="image" width="180" />
-                </template>
-              </v-img>
-
-              <div class="d-flex flex-column justify-space-between w-100">
-                <div class="d-flex justify-space-between align-center">
-                  <div class="text-subtitle-1 font-weight-medium text-truncate">
-                    {{ item.title || 'Sem título' }}
+                <template v-if="item.isFolder">
+                  <v-icon class="mr-4" color="primary" size="48">mdi-folder</v-icon>
+                  <div>
+                    <div class="text-subtitle-1 font-weight-medium">{{ item.name }}</div>
+                    <div class="text-caption text-grey-darken-1">{{ item.count }} vídeos</div>
                   </div>
-                  <v-chip
-                    class="ml-2 d-flex align-center"
-                    :color="item.status === 'CONVERTING' ? 'blue' : 'green'"
-                    label
-                    size="small"
+                </template>
+
+                <template v-else>
+                  <v-img
+                    aspect-ratio="16/8"
+                    class="rounded mr-4"
+                    cover
+                    height="60"
+                    :src="item.thumbnail"
+                    width="120"
                   >
-                    <template v-if="item.status === 'CONVERTING'">
-                      <v-progress-circular
-                        class="mr-2"
-                        color="white"
-                        indeterminate
-                        size="14"
-                        width="2"
-                      />
-                      Convertendo...
+                    <template #placeholder>
+                      <v-skeleton-loader height="100" type="image" width="180" />
                     </template>
-                    <template v-else>
-                      {{ item.status }}
-                    </template>
-                  </v-chip>
-                </div>
+                  </v-img>
 
-                <div class="d-flex justify-space-between text-caption mt-2">
-                  <div><v-icon class="mr-1" size="16">mdi-calendar</v-icon>{{ formatDate(item.created_at) }}</div>
-                  <div><v-icon class="mr-1" size="16">mdi-clock</v-icon>{{ formatLength(item.length) }}</div>
-                  <div><v-icon class="mr-1" size="16">mdi-database</v-icon>{{ formatSize(item.storage_size) }}</div>
-                </div>
-              </div>
-            </template>
-          </v-card>
+                  <div class="d-flex flex-column justify-space-between w-100">
+                    <div class="d-flex justify-space-between align-center">
+                      <div class="text-subtitle-1 font-weight-medium text-truncate">
+                        {{ item.title || 'Sem título' }}
+                      </div>
+                      <v-chip
+                        class="ml-2 d-flex align-center"
+                        :color="item.status === 'CONVERTING' ? 'blue' : 'green'"
+                        label
+                        size="small"
+                      >
+                        <template v-if="item.status === 'CONVERTING'">
+                          <v-progress-circular
+                            class="mr-2"
+                            color="white"
+                            indeterminate
+                            size="14"
+                            width="2"
+                          />
+                          Convertendo...
+                        </template>
+                        <template v-else>
+                          {{ item.status }}
+                        </template>
+                      </v-chip>
+                    </div>
+                    <div class="d-flex justify-space-between text-caption mt-2">
+                      <div><v-icon class="mr-1" size="16">mdi-calendar</v-icon>{{ formatDate(item.created_at) }}</div>
+                      <div><v-icon class="mr-1" size="16">mdi-clock</v-icon>{{ formatLength(item.length) }}</div>
+                      <div><v-icon class="mr-1" size="16">mdi-database</v-icon>{{ formatSize(item.storage_size) }}</div>
+                    </div>
+                  </div>
+                </template>
+              </v-card>
+            </v-col>
+          </v-responsive>
 
+          <!-- MOBILE -->
+          <v-responsive class="d-sm-none w-100">
+            <v-row dense>
+              <v-col
+                v-for="item in paginatedItems"
+                :key="'mobile-' + item.id"
+                class="d-flex"
+                cols="6"
+              >
+                <v-card
+                  class="pa-3 d-flex flex-column justify-space-between align-center text-center w-100"
+                  color="grey-lighten-4"
+                  :hover="true"
+                  style="height: 200px;"
+                  @click="handleRowClick({ item })"
+                >
+                  <template v-if="item.isFolder">
+                    <v-icon color="primary" size="48">mdi-folder</v-icon>
+                    <div class="mt-2 text-caption font-weight-medium">
+                      {{ item.name }}
+                    </div>
+                    <div class="text-caption text-grey-darken-1">
+                      {{ item.count }} vídeos
+                    </div>
+                  </template>
+
+                  <template v-else>
+                    <v-img
+                      class="rounded"
+                      cover
+                      height="100"
+                      :src="item.thumbnail"
+                      width="100%"
+                    >
+                      <template #placeholder>
+                        <v-skeleton-loader height="100" type="image" width="100%" />
+                      </template>
+                    </v-img>
+                    <div class="d-flex flex-column w-100">
+                      <div class="mt-2 text-caption font-weight-medium text-truncate w-100">
+                        {{ item.title || 'Sem título' }}
+                      </div>
+                      <v-chip
+                        class="ml-2 d-flex align-center"
+                        :color="item.status === 'CONVERTING' ? 'blue' : 'green'"
+                        label
+                        size="small"
+                      >
+                        <template v-if="item.status === 'CONVERTING'">
+                          <v-progress-circular
+                            class="mr-2"
+                            color="white"
+                            indeterminate
+                            size="14"
+                            width="2"
+                          />
+                          Convertendo...
+                        </template>
+                        <template v-else>
+                          {{ item.status }}
+                        </template>
+                      </v-chip>
+                      <div><v-icon class="mr-1" size="16">mdi-clock</v-icon>{{ formatLength(item.length) }}</div>
+                    </div>
+                  </template>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-responsive>
         </v-row>
+        <v-row
+          v-else-if="isLoading"
+          class="px-6 py-10 d-flex flex-column align-center justify-center text-center "
+          style="min-height: 300px; height: 60vh; max-height: 90vh;"
+        >
+          <v-progress-circular indeterminate />
+        </v-row>
+        <v-row
+          v-else
+          class="px-6 py-10 d-flex flex-column align-center justify-center text-center "
+          style="min-height: 300px; height: 60vh; max-height: 90vh;"
+        >
+          <v-icon class="mb-3" color="grey-darken-1" size="64">mdi-folder-open</v-icon>
+          <div class="text-subtitle-1 font-weight-medium mb-1">Nenhum conteúdo encontrado</div>
+        </v-row>
+
         <v-pagination
           v-model="page"
-          class="mt-4 align-self-center"
+          class="py-4 align-self-center"
           :length="Math.ceil(filteredItems.length / itemsPerPage)"
+          variant="tonal"
         />
-
       </v-card>
     </div>
 
@@ -245,7 +350,6 @@
 
   onMounted (async () => {
     const token = localStorage.getItem('token')
-    console.log(token)
 
     const publicPages = ['/login', '/unauthorized']
     if (!token && !publicPages.includes(router.currentRoute.value.path)) {
@@ -255,7 +359,9 @@
     const { folderId, history } = loadNavigation()
     currentFolder.value = folderId
     folderHistory.value = history || []
+
     await fetchFolders(currentFolder.value)
+
     await fetchVideos(currentFolder.value)
     isLoading.value = false
   })
@@ -298,6 +404,7 @@
         : cached
     } else {
       try {
+        console.log('Aqui')
         const { data } = await getFolders(parentFolderId ? { parent_folder_id: parentFolderId } : {})
         const fetchedFolders = data.folders || []
         folders.value = parentFolderId === null
